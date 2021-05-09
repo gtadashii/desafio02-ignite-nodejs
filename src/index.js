60fcda78c4d8f96b27ed515f7459e9d9510d3bb1
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-05-09 12:01:43 -0300

    All tests passed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
-  const user = users.find((user) => user.username = username);
+  const user = users.find((user) => user.username === username);
 
   if (!user) { return response.status(404).json({ error: "User not found!" }); }
 
